5792cf57b21a776609be900eb1fcde9b2235c17e
restreposa <sarestrepo3@gmail.com>
2024-08-12 10:40:03 -0500

    se crea la ruta de logueo y la instalacion de modulo espress-session

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 const express = require ('express');
 const path = require ('path');
+const session = require('express-session');
+const morgan = require('morgan');
 
 const app= express();
 const PORT = process.env.PORT || 3000;
 
+
+
+
+// Configurar la sesion
+app.use(session({
+    secret: 'mySecretKey',
+    resave: false,
+    saveUninitialized: true
+}));
 
 // resolver las rutas statics 
 app.use (express.static("public"));
@@ -22,4 +33,4 @@ const dbconfig ={
 }
 
 
-module.exports = {app, PORT, dbconfig };
+module.exports = {app, PORT, dbconfig,session };
